5cd7e62453a94513b3d2da70ed535e66548d1dd4
shad <sa@sorokin.org>
2018-03-16 00:17:14 +0200

    edit start.sh, conditions

--- start.sh
+++ start.sh
@@ -2,9 +2,14 @@
 
 /usr/bin/echo >/var/log/ipsec.log
 /usr/bin/rm -f /var/run/charon.pid
-/bin/sed -i -e "s:rightsourceip=192.168.95.0/24:rightsourceip=$VPN_SUBNET:g" /etc/strongswan/ipsec.conf
-/usr/bin/echo ": PSK \"$VPN_PSK\"" >/etc/strongswan/ipsec.secrets
-/usr/bin/echo "$VPN_USER : XAUTH \"$VPN_PASS\"" >>/etc/strongswan/ipsec.secrets
+
+	if [ ! -f "/firsttime" ]; then
+		/bin/sed -i -e "s:rightsourceip=192.168.95.0/24:rightsourceip=$VPN_SUBNET:g" /etc/strongswan/ipsec.conf
+		/usr/bin/echo ": PSK \"$VPN_PSK\"" >/etc/strongswan/ipsec.secrets
+		/usr/bin/echo "$VPN_USER : XAUTH \"$VPN_PASS\"" >>/etc/strongswan/ipsec.secrets
+		/usr/bin/touch /firsttime	
+	fi
+
 /usr/sbin/strongswan start --nofork &
 /bin/sleep 20
 /sbin/iptables -t nat -A POSTROUTING -s $VPN_SUBNET -j MASQUERADE
