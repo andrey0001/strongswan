353c97050e43d4ad5f945326da331d9cb4db454e
shad <sa@sorokin.org>
2018-03-15 21:35:58 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/usr/bin/echo >/var/log/ipsec.log
+/usr/bin/rm -f /var/run/charon.pid
+/bin/sed -i -e "s:rightsourceip=192.168.95.0/24:rightsourceip=$VPN_SUBNET:g" /etc/strongswan/ipsec.conf
+/usr/bin/echo ": PSK \"$VPN_PSK\"" >/etc/strongswan/ipsec.secrets
+/usr/bin/echo "$VPN_USER : XAUTH \"$VPN_PASS\"" >>/etc/strongswan/ipsec.secrets
+/usr/sbin/strongswan start --nofork &
+/bin/sleep 20
+/sbin/iptables -t nat -A POSTROUTING -s $VPN_SUBNET -j MASQUERADE
+/sbin/sysctl -w net.ipv4.ip_forward=1
+
+[ "$1" ] && exec "$@"
+
