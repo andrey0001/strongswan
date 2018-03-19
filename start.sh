876bffdcc394584327110d2fdd9acec0c65d336f
shad <sa@sorokin.org>
2018-03-20 01:28:37 +0200

    edit start.sh, network tuning

--- start.sh
+++ start.sh
@@ -14,6 +14,13 @@
 /bin/sleep 30
 /sbin/iptables -t nat -A POSTROUTING -s $VPN_SUBNET -j MASQUERADE
 /sbin/sysctl -w net.ipv4.ip_forward=1
+/sbin/sysctl -w net.ipv4.conf.all.rp_filter=2
+
+	for each in /proc/sys/net/ipv4/conf/*
+	do
+		echo 0 > $each/accept_redirects
+		echo 0 > $each/send_redirects
+	done
 
 [ "$1" ] && exec "$@"
 
